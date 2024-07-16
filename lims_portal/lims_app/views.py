8b2116ff8b64b5709417628e4bd310214f595fea
Tinomutenda <tynoenyamhingura@gmail.com>
2024-07-16 10:42:59 +0200

    routing

--- lims_portal/lims_app/views.py
+++ lims_portal/lims_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib import admin
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Hello world")
+
+def shopping(request):
+    return HttpResponse("Welcome to shopping")
